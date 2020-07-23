cd2dc0aa5b36915f520c128e431a26cabcd8ace0
Nico Audy <nicoaudy1994@gmail.com>
2020-07-23 14:22:36 +0700

    simple print

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use std::io::{{stdout}}
+use std::io::{{stdout, stdin, Write}};
 
 fn read(input: &mut String) {
     stdout().flush().expect("failed to flush");
+    stdin().read_line(input).expect("failed to read");
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello Calc!");
+
+    let mut num_1 = String::new();
+    let mut num_2 = String::new();
+    let mut operators = String::new();
+
+    print!("What is the first number?: ");
+    read(&mut num_1);
+
+    print!("What is the second number?: ");
+    read(&mut num_2);
+
+    print!("What operation would you like to do? [+-*/]: ");
+    read(&mut operators);
 }
